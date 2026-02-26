22fb116a6bd15dabaca5627fafeb6a15b7df96e5
ChrishanAloka <chrishan2002aloka@gmail.com>
2026-02-26 08:52:47 +0530

    Update frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://emeenainteriordesigners.onrender.com',
+        target: 'https://emeenainteriordesigners-68tv.onrender.com',
         changeOrigin: true
       }
     }
